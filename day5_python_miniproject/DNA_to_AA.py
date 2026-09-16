0ccc522529b70a06d0d3dec48b39f4001e413772
Tristan McDonnell <tristanmcdonnell@Tristan-is-this-ur-M5-Pro.local>
2026-09-16 10:43:56 -0700

    day 5 intro to prog

---
+++ day5_python_miniproject/DNA_to_AA.py
@@ -0,0 +1,46 @@
+# RNA to AA dictionary:
+genetic_code = {
+    'AUG': 'M', 
+    'UUU': 'F', 'UUC': 'F',
+    'UUA': 'L', 'UUG': 'L', 'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I',
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S', 'AGU': 'S', 'AGC': 'S',
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'UAU': 'Y', 'UAC': 'Y',
+    'CAU': 'H', 'CAC': 'H',
+    'CAA': 'Q', 'CAG': 'Q',
+    'AAU': 'N', 'AAC': 'N',
+    'AAA': 'K', 'AAG': 'K',
+    'GAU': 'D', 'GAC': 'D',
+    'GAA': 'E', 'GAG': 'E',
+    'UGU': 'C', 'UGC': 'C',
+    'UGG': 'W',
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'AGA': 'R', 'AGG': 'R',
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    'UAA': 'Stop', 'UAG': 'Stop', 'UGA': 'Stop'
+}
+
+# read DNA
+with open('mystery_protein.fa', "r") as f:
+    lines = f.readlines()
+
+header = lines[0]
+dna_sequence = lines[1].strip()
+print("Header: ", header.strip())
+print("DNA Sequence:\n", dna_sequence)
+
+#make rna from dna
+rna_sequence = dna_sequence.replace('T', 'U')
+print(f"RNA Sequence:\n", rna_sequence)
+
+#Split to codons
+aa_sequence = ""
+start_position = 0
+while start_position < len(rna_sequence):
+    chunk = rna_sequence[start_position:start_position + 3]
+    aa_sequence += genetic_code[chunk]
+    start_position += 3
+print(f"AA Sequence:\n",aa_sequence)
